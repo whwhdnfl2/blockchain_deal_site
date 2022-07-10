2796d94646f26fad3f64ee6a9a17c2fde728372b
Cha minjun <whwhdnfl2@naver.com>
2022-07-10 22:56:23 +0900

    [Add] fabric-samples 폴더를 add함.

--- test.js
+++
@@ -1,5 +0,0 @@
-function concat(left, right) {
-    return left + right;
-}
-var hello = concat("hello", "world");
-console.log(hello);
--- test.ts
+++
@@ -1,5 +0,0 @@
-function concat(left:string , right:string):string{ 
-      return left+right; 
-    } 
-    let hello:string = concat("hello", "world"); 
-    console.log(hello);
