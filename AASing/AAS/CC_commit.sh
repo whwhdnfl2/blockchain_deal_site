1688a136a4f9542c12342fd705a58d74889cd7b5
kimcharless <kimqhahqhah@naver.com>
2022-08-04 17:45:25 +0900

    People 채널 Policy 추가

--- AASing/AAS/CC_commit.sh
+++ AASing/AAS/CC_commit.sh
@@ -8,32 +8,32 @@ export FABRIC_CFG_PATH=$PWD/../config/
 #export CC_PACKAGE_ID="market_1.0:a61c9a479a5cc51673cba6a0b4ca18f4209497c1fad320324c3a0463992242fc"
 export CC_VERSION="1.0"
 getcheck(){
-
+    export CC_END_POLICY="--signature-policy OR('sellerMSP.peer','buyerMSP.peer')" 
     CC_NAME="people"
     CC_VERSION="1.0"
     setGlobals seller
     peer lifecycle chaincode queryinstalled >&log.txt
-    peer lifecycle chaincode checkcommitreadiness --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+    peer lifecycle chaincode checkcommitreadiness --channelID people ${CC_END_POLICY} --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
     CC_PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
     
-    peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID ${CC_NAME} --name ${CC_NAME} --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-    peer lifecycle chaincode checkcommitreadiness --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+    peer lifecycle chaincode approveformyorg -o localhost:7050 ${CC_END_POLICY} --ordererTLSHostnameOverride orderer.example.com --channelID ${CC_NAME} --name ${CC_NAME} --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    peer lifecycle chaincode checkcommitreadiness --channelID people ${CC_END_POLICY} --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
 
     cat log.txt
 
     setGlobals buyer
     peer lifecycle chaincode queryinstalled >&log.txt
-    peer lifecycle chaincode checkcommitreadiness --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+    peer lifecycle chaincode checkcommitreadiness --channelID people ${CC_END_POLICY} --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
     CC_PACKAGE_ID=$(sed -n "/${CC_NAME}_${CC_VERSION}/{s/^Package ID: //; s/, Label:.*$//; p;}" log.txt)
-    peer lifecycle chaincode approveformyorg -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID ${CC_NAME} --name ${CC_NAME} --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
-    peer lifecycle chaincode checkcommitreadiness --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
+    peer lifecycle chaincode approveformyorg -o localhost:7050 ${CC_END_POLICY} --ordererTLSHostnameOverride orderer.example.com --channelID ${CC_NAME} --name ${CC_NAME} --version 1.0 --package-id $CC_PACKAGE_ID --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    peer lifecycle chaincode checkcommitreadiness --channelID people ${CC_END_POLICY} --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --output json
 
     cat log.txt
     
     infoln "채널 people 커밋"
-    peer lifecycle chaincode commit -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/seller.example.com/peers/peer0.seller.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/buyer.example.com/peers/peer0.buyer.example.com/tls/ca.crt"
+    peer lifecycle chaincode commit -o localhost:7050 ${CC_END_POLICY} --ordererTLSHostnameOverride orderer.example.com --channelID people --name people --version 1.0 --sequence 1 --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" --peerAddresses localhost:7051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/seller.example.com/peers/peer0.seller.example.com/tls/ca.crt" --peerAddresses localhost:9051 --tlsRootCertFiles "${PWD}/organizations/peerOrganizations/buyer.example.com/peers/peer0.buyer.example.com/tls/ca.crt"
     infoln "채널 people 확인"
-    peer lifecycle chaincode querycommitted --channelID people  --name people --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
+    peer lifecycle chaincode querycommitted --channelID people --name people --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
     
 
     setGlobals tax
