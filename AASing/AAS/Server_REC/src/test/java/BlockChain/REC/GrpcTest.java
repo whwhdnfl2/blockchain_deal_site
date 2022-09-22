d31f9f953ea2dfda388b6af1396bde07f06def11
김재현 <kimqhahqhah@naver.com>
2022-09-22 20:07:26 +0900

    Fix : GrpcTest EasilyConnect에 맞도록 수정

--- AASing/AAS/Server_REC/src/test/java/BlockChain/REC/GrpcTest.java
+++ AASing/AAS/Server_REC/src/test/java/BlockChain/REC/GrpcTest.java
@@ -2,18 +2,14 @@ package BlockChain.REC;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import BlockChain.REC.connection.BlockChainConnect;
-import BlockChain.REC.connection.Grpc;
 import BlockChain.REC.domain.Account;
 import BlockChain.REC.dto.MemberDto;
 import BlockChain.REC.repository.AccountRepository;
-import BlockChain.REC.service.AccountService;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Transactional(readOnly = true)
 public class GrpcTest {
