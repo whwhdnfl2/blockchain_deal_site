45e7c2218eba770b8d0c937e8411f9cf6cf25c2b
김재현 <kimqhahqhah@naver.com>
2022-09-22 20:06:10 +0900

    Fix : 데이터 구조 수정

--- AASing/AAS/Server_REC/src/main/java/BlockChain/REC/dto/AssetDto.java
+++ AASing/AAS/Server_REC/src/main/java/BlockChain/REC/dto/AssetDto.java
@@ -9,18 +9,103 @@ import lombok.Data;
 public class AssetDto {
   private String docType;
   private String ID;
-  private String owner;
-  private Long REC;
-  private Long KRW;
-  private String Role;
+  private int REC;
+  private int KRW;
+  private int RTKRW;
+  private String RTpeople;
 
+    @Override
+    public String toString() {
+        return "AssetDto{" +
+                "docType='" + docType + '\'' +
+                ", ID='" + ID + '\'' +
+                ", REC=" + REC +
+                ", KRW=" + KRW +
+                ", RTKRW=" + RTKRW +
+                ", RTpeople='" + RTpeople + '\'' +
+                ", Role='" + Role + '\'' +
+                ", RTREC=" + RTREC +
+                '}';
+    }
+
+    private String Role;
+  private int RTREC;
 
   public AssetDto(JsonObject asset) {
         this.docType = asset.get("docType").getAsString();
         this.ID = asset.get("ID").getAsString();
-        this.owner = asset.get("owner").getAsString();
-        this.REC = asset.get("REC").getAsLong();
-        this.KRW = asset.get("KRW").getAsLong();
+        this.REC = asset.get("REC").getAsInt();
+        this.KRW = asset.get("KRW").getAsInt();
         this.Role = asset.get("Role").getAsString();
+        this.RTKRW = asset.get("RTKRW").getAsInt();
+        this.RTREC = asset.get("RTREC").getAsInt();
+        this.RTpeople = asset.get("RTpeople").getAsString();
   }
+    public String getDocType() {
+        return docType;
+    }
+
+    public void setDocType(String docType) {
+        this.docType = docType;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public AssetDto() {
+
+    }
+
+    public int getREC() {
+        return REC;
+    }
+
+    public void setREC(int REC) {
+        this.REC = REC;
+    }
+
+    public int getKRW() {
+        return KRW;
+    }
+
+    public void setKRW(int KRW) {
+        this.KRW = KRW;
+    }
+
+    public int getRTKRW() {
+        return RTKRW;
+    }
+
+    public void setRTKRW(int RTKRW) {
+        this.RTKRW = RTKRW;
+    }
+
+    public String getRTpeople() {
+        return RTpeople;
+    }
+
+    public void setRTpeople(String RTpeople) {
+        this.RTpeople = RTpeople;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String role) {
+        Role = role;
+    }
+
+    public int getRTREC() {
+        return RTREC;
+    }
+
+    public void setRTREC(int RTREC) {
+        this.RTREC = RTREC;
+    }
 }
--- AASing/AAS/Server_REC/src/main/java/BlockChain/REC/dto/MemberDto.java
+++ AASing/AAS/Server_REC/src/main/java/BlockChain/REC/dto/MemberDto.java
@@ -27,22 +27,29 @@ public class MemberDto {
   private String peerEndpoint;
   private String overrideAuth;
 
+  private String ID;
+  private String User;
+
   public MemberDto(Account account) {
     this.original_id = account.getMsp_id();
+    this.ID = account.getUser_id();
+    this.User = this.ID.substring(this.ID.length()-5);
     this.mspID = String.format("%sMSP", account.getMsp_id());
     this.channelName = account.getChannel_name();
     this.chaincodeName = account.getChaincode_name();
     this.cryptoPath = Paths.get("..","organizations", "peerOrganizations",
         String.format("%s.example.com", this.original_id));
     this.certPath = this.cryptoPath.resolve(Paths.get("users",
-        String.format("User1@%s.example.com", this.original_id), "msp", "signcerts",
-        String.format("User1@%s.example.com-cert.pem", this.original_id)));
-    this.keyDirPath = this.cryptoPath.resolve(Paths.get("users", String.format("User1@%s.example.com", this.original_id), "msp", "keystore"));;
+        String.format("%s@%s.example.com",this.User, this.original_id), "msp", "signcerts",
+        String.format("%s@%s.example.com-cert.pem",this.User, this.original_id)));
+    this.keyDirPath = this.cryptoPath.resolve(Paths.get("users", String.format("%s@%s.example.com",this.User, this.original_id), "msp", "keystore"));;
     this.tlsCertPath = this.cryptoPath.resolve(Paths.get("peers",
         String.format("peer0.%s.example.com", this.original_id), "tls", "ca.crt"));;
     this.peerEndpoint = account.getPeer_end_point();
     this.overrideAuth = String.format("peer0.%s.example.com", this.original_id);
 
+    System.out.println("ID : " + this.ID);
+    System.out.println("User : "+this.User);
     System.out.println("original_id : " + original_id);
     System.out.println("mspID : " + mspID);
     System.out.println("cryptoPath : " + cryptoPath + "권한 : " + this.cryptoPath.toFile().canRead());
@@ -53,5 +60,24 @@ public class MemberDto {
     System.out.println("chaincodeName : " + chaincodeName);
     System.out.println("peerEndpoint : " + peerEndpoint);
     System.out.println("overrideAuth : " + overrideAuth);
+  }
+
+  @Override
+  public String toString() {
+    return "MemberDto{" +
+            "original_id='" + original_id + '\'' +
+            ", mspID='" + mspID + '\'' +
+            ", channelName='" + channelName + '\'' +
+            ", chaincodeName='" + chaincodeName + '\'' +
+            ", cryptoPath=" + cryptoPath +
+            ", certPath=" + certPath +
+            ", keyDirPath=" + keyDirPath +
+            ", tlsCertPath=" + tlsCertPath +
+            ", basePath=" + basePath +
+            ", peerEndpoint='" + peerEndpoint + '\'' +
+            ", overrideAuth='" + overrideAuth + '\'' +
+            ", ID='" + ID + '\'' +
+            ", User='" + User + '\'' +
+            '}';
   }
 }
