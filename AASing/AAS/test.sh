4802c7413765d45e987a93b91727253e71b4b024
Minjun Cha <whwhdnfl2@naver.com>
2022-09-24 14:53:50 +0900

    [Sell] 만듦

--- AASing/AAS/test.sh
+++ AASing/AAS/test.sh
@@ -14,20 +14,20 @@ export FABRIC_CFG_PATH=$PWD/../config/
 
 
 # setGlobals buyer
-# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","asset2","fuck","5","25000","done"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["UpdateAsset","Buyer_User2","5","100000","buyer"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["UpdateAsset","Buyer_User1","0","0","buyer"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["UpdateAsset","Seller_User2","0","0","seller"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["UpdateAsset","Seller_User1","0","0","seller"]}' 
+# peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","asset2","fuck","5","25000","done"]}' 
+peer chaincode invoke -o localhost:7050 -ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","Buyer_User2","5","100000","buyer"]}' 
+peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","Buyer_User1","0","0","buyer"]}' 
+peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","Seller_User2","0","0","seller"]}' 
+peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C people -n people -c '{"Args":["CreateAsset","Seller_User1","0","0","seller"]}' 
 # peer chaincode query -C people -n people -c '{"Args":["GetAllAssets"]}' 
 
 setGlobals tax
-# peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C revenue -n revenue -c '{"Args":["CreateAsset","TAX","REVENUE","3"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C revenue -n revenue -c '{"Args":["UpdateAsset","TAX","REVENUE","6"]}' 
-peer chaincode query -C revenue -n revenue -c '{"Args":["ReadAsset","TAX"]}' 
+# peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C revenue -n revenue -c '{"Args":["CreateAsset","TAX","REVENUE","3"]}' 
+#peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C revenue -n revenue -c '{"Args":["UpdateAsset","TAX","REVENUE","6"]}' 
+#peer chaincode query -C revenue -n revenue -c '{"Args":["ReadAsset","TAX"]}' 
 
 
 setGlobals koreapower
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C market -n market -c '{"Args":["UpdateAsset","Seller_User1","Seller_User1"," ","10","8000","SALE","time"]}' 
-peer chaincode invoke -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com --tls --cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C market -n market -c '{"Args":["UpdateAsset","Seller_User2","Seller_User2"," ","5","10000","SALE","time"]}' 
+#peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C market -n market -c '{"Args":["UpdateAsset","Seller_User1","Seller_User1"," ","10","8000","SALE","time"]}' 
+#peer chaincode invoke -o localhost:7050 —ordererTLSHostnameOverride orderer.example.com —tls —cafile "${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem" -C market -n market -c '{"Args":["UpdateAsset","Seller_User2","Seller_User2"," ","5","10000","SALE","time"]}' 
 # peer chaincode query -C market -n market -c '{"Args":["GetAllAssets"]}' 
--- web/test/src/components/Sell/SellPage.js
+++ web/test/src/components/Sell/SellPage.js
@@ -46,22 +46,52 @@ const SellPage = (props) => {
   };
 
   async function postSellData(){
-    const SellData = {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    const SellRECData = JSON.stringify({
       id: props.myID,
       rec: insertRec,
-    }
+    });
+
     try{
       const response = await fetch(`/api/Market/upREC`, {
         method: 'POST',
-        body: JSON.stringify(SellData)
+        headers: myHeaders,
+        body: SellRECData,
+        redirect: 'follow'
       });
       if(!response.ok){
-        throw new Error('market등록 실패')
+        throw new Error('market등록 실패1')
       }
       const data = await response.json();
+      if(data === 'fail'){
+        console.log("이미 시장에 있음");
+      }
       props.onMyRec(props.myRec - insertRec);
       console.log(JSON.stringify(data));
-      console.log("postsell");
+      console.log("postsell1");
+    }catch(error){
+      console.log(error.message);
+      return;
+    }
+    const SellKRWData = JSON.stringify({
+      id: props.myID,
+      krw: insertPrice,
+    });
+    try{
+      const response = await fetch(`/api/Market/upKRW`, {
+        method: 'POST',
+        headers: myHeaders,
+        body: SellKRWData,
+        redirect: 'follow'
+      });
+      if(!response.ok){
+        throw new Error('market등록 실패2')
+      }
+      const data = await response.json();
+      console.log(JSON.stringify(data));
+      console.log("postsell2");
     }catch(error){
       console.log(error.message);
     }
