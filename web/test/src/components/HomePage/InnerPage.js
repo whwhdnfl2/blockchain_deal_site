9781e4651d4f9e8201828b306434b531abf5944d
Minjun Cha <whwhdnfl2@naver.com>
2022-09-25 02:22:22 +0900

    classname

--- web/test/src/components/HomePage/InnerPage.js
+++ web/test/src/components/HomePage/InnerPage.js
@@ -9,19 +9,19 @@ const InnerPage = (props) => {
         <React.Fragment>
             <div class="parent">
                 <Card>
-                    <h2 class="good" >보유 자산</h2>
+                    <h2 className="good" >보유 자산</h2>
                     <h3>{props.asset}</h3>
                 </Card>
                 <Card>
-                    <h2 class="good" >보유 REC</h2>
+                    <h2 className="good" >보유 REC</h2>
                     <h3>{props.rec}</h3>
                 </Card>
                 <Card>
-                    <h2 class="good" >최근 거래한 자산 양</h2>
+                    <h2 className="good" >최근 거래한 자산 양</h2>
                     <h3>{props.rKRW}</h3>
                 </Card>
                 <Card>
-                    <h2 class="good" >최근 거래한 REC 양</h2>
+                    <h2 className="good" >최근 거래한 REC 양</h2>
                     <h3>{props.rREC}</h3>
                 </Card>
             </div>
