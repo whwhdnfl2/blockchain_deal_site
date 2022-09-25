f98c1c7d679a413a2503207f337d18ad968c808a
Minjun Cha <whwhdnfl2@naver.com>
2022-09-25 15:58:17 +0900

    홈페이지 업데이트

--- web/test/src/components/HomePage/FirstPage.js
+++ web/test/src/components/HomePage/FirstPage.js
@@ -1,9 +1,123 @@
-import React from "react";
 
-const FirstPage = () => {
+import classes from "./good.module.css";
+import Card from "./Card"
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+// const data = [
+//     {
+//       name: 'Page A',
+//       uv: 4000,
+//       pv: 2400,
+//       amt: 2400,
+//     },
+//     {
+//       name: 'Page B',
+//       uv: 3000,
+//       pv: 1398,
+//       amt: 2210,
+//     },
+//     {
+//       name: 'Page C',
+//       uv: 2000,
+//       pv: 9800,
+//       amt: 2290,
+//     },
+//     {
+//       name: 'Page D',
+//       uv: 2780,
+//       pv: 3908,
+//       amt: 2000,
+//     },
+//     {
+//       name: 'Page E',
+//       uv: 1890,
+//       pv: 4800,
+//       amt: 2181,
+//     },
+//     {
+//       name: 'Page F',
+//       uv: 2390,
+//       pv: 3800,
+//       amt: 2500,
+//     },
+//     {
+//       name: 'Page G',
+//       uv: 3490,
+//       pv: 4300,
+//       amt: 2100,
+//     },
+//   ];
+
+  export default class FirstPage extends PureComponent  {
+    render(){
     return (
-        <h1>firstpage234</h1>
-    );
+        <React.Fragment>
+            <div className={classes.parent}>
+                <Card>
+                    <div className={classes.good}>
+                        <h2>보유 자산</h2>
+                        <br/>
+                        {/* <p className={classes.nice}>{props.asset}</p> */}
+                        <p className={classes.nice}>asd`</p>
+                    </div>
+                </Card>
+                <Card>
+                    <div className={classes.good}>
+                        <h2>보유 REC</h2>
+                        <br/>
+                        {/* <p className={classes.nice}>{props.rec}</p> */}
+                        <p className={classes.nice}>asd</p>
+                    </div>
+
+                </Card>
+                <Card>
+                    <div className={classes.good}>
+                        <h2>최근 거래한 자산 양</h2>
+                        <br/>
+                        {/* <p className={classes.nice}>{props.rKRW}</p> */}
+                        <p className={classes.nice}>asd</p>
+                    </div>
+
+                </Card>
+                <Card>
+                    <div className={classes.good}>
+                        <h2>최근 거래한 REC 양</h2>
+                        <br/>
+                        {/* <p className={classes.nice}>{props.rREC}</p> */}
+                        <p className={classes.nice}>asd</p>
+                    </div>
+
+                </Card>
+            </div>
+<br></br>
+<br></br>
+
+
+
+        <LineChart
+          width={1210}
+          height={500}
+          data={this.props.AllInformationRow}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+
+
+        </React.Fragment>
+      );
+        }
 }
 
-export default FirstPage;
--- web/test/src/components/HomePage/InnerPage.js
+++ web/test/src/components/HomePage/InnerPage.js
@@ -9,19 +9,31 @@ const InnerPage = (props) => {
             <div className={classes.parent}>
                 <Card>
                     <h2 className={classes.good} >보유 자산</h2>
-                    <h3>{props.asset}</h3>
+                    <div/>
+                    {/* <h3>{props.asset}</h3> */}
+                    <div className={classes.nice}>
+                        <h3>{props.asset}</h3>
+                    </div>
+
+
                 </Card>
                 <Card>
                     <h2 className={classes.good} >보유 REC</h2>
+                    <div/>
+                    {/* <h3>{props.rec}</h3> */}
                     <h3>{props.rec}</h3>
                 </Card>
                 <Card>
                     <h2 className={classes.good} >최근 거래한 자산 양</h2>
-                    <h3>{props.rKRW}</h3>
+                    <div/>
+                    {/* <h3>{props.rKRW}</h3> */}
+                    <h3>{props.rec}</h3>
                 </Card>
                 <Card>
                     <h2 className={classes.good} >최근 거래한 REC 양</h2>
-                    <h3>{props.rREC}</h3>
+                    <div/>
+                    {/* <h3>{props.rREC}</h3> */}
+                    <h3>{props.rec}</h3>
                 </Card>
             </div>
         </React.Fragment>
